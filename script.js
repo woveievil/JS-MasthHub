2ff1723be830a8c08b5db0064ccddb799609e71a
woveievil <woveievil@gmail.com>
2023-02-24 17:16:49 +0300

    first commit

--- script.js
+++ script.js
@@ -90,4 +90,81 @@ const eventFunc = (e) => {
 for (let i = 0; i < choosedEl.length; i++) {
     choosedEl[i].addEventListener("click", eventFunc)
 }
-choosedEl[2].removeEventListener("click", eventFunc)
+
+
+// choosedEl[2].removeEventListener("click", eventFunc)
+
+// const timeIsOver = () => {
+//     alert("Время вышло")
+// }
+// setTimeout(timeIsOver ,2000)
+// const alarm = setInterval(timeIsOver, 3000)
+
+// const alarm = setInterval(() => {
+//     let wantToSleep = confirm("Хотите ли вы спать?")
+//     if (wantToSleep) {
+//         console.log("tic")
+//     } else {
+//         clearInterval(alarm)
+//     }
+// }, 3000)
+
+
+// console.log("1")
+// setTimeout(() => {
+//     console.log("2")
+// }, 0)
+
+// console.log("3")
+
+const postsBlock = document.querySelector(".posts_block-container")
+const showPostsBTN = document.querySelector(".posts_block button")
+
+const func = () => 5
+
+function addPost(title, body) {
+    const postTitle = document.createElement("h3")
+    const postBody = document.createElement("span")
+    const postItem = document.createElement("p")
+
+    postTitle.innerText = title
+    postBody.innerText = body
+
+    postItem.append(postTitle, postBody)
+    postsBlock.append(postItem)
+}
+
+function getPosts () {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(res => res.json())
+    .then(data => {
+        for (el of data) {
+            addPost(el.title, el.body)
+        }
+        // addPost(data[7].title, data[7].body)
+    })
+    .catch(err => console.log(err.message))
+}
+
+
+// function createPost(title, body, userId) {
+//     fetch("https://jsonplaceholder.typicode.com/posts", {
+//         method: 'POST',
+//         body: JSON.stringify({
+//             title,
+//             body,
+//             userId,
+//         }),
+//         headers: {
+//             'Content-type': 'application/json; charset=UTF-8',
+//         }
+//     })
+//         .then(res => {
+//             console.log(res)
+//             return res.json()
+//         })
+//         .catch(err => console.log(err.message))
+// }
+// createPost("title", "body", 15)
+
+showPostsBTN.onclick = () => {getPosts()}
